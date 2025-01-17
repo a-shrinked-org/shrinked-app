b9828daf9bc4b409401b6ccc69978ea16f26bce8
damnventures <hi@damn.vc>
2025-01-16 19:35:55 -0500

    no idea

--- src/providers/data-provider/index.ts
+++ src/providers/data-provider/index.ts
@@ -1,10 +1,47 @@
 "use client";
 import dataProviderSimpleRest from "@refinedev/simple-rest";
+import { DataProvider } from "@refinedev/core";
 
 const API_URL = "https://api.fake-rest.refine.dev";
 
-export const dataProvider = dataProviderSimpleRest(API_URL, {
-  mapResources: {
-	jobs: 'blog_posts'
+const simpleRestProvider = dataProviderSimpleRest(API_URL);
+
+export const dataProvider: DataProvider = {
+  ...simpleRestProvider,
+  getList: async ({ resource, ...params }) => {
+	return simpleRestProvider.getList({ 
+	  resource: resource === "jobs" ? "blog_posts" : resource,
+	  ...params 
+	});
+  },
+  getOne: async ({ resource, ...params }) => {
+	return simpleRestProvider.getOne({ 
+	  resource: resource === "jobs" ? "blog_posts" : resource,
+	  ...params 
+	});
+  },
+  create: async ({ resource, ...params }) => {
+	return simpleRestProvider.create({ 
+	  resource: resource === "jobs" ? "blog_posts" : resource,
+	  ...params 
+	});
+  },
+  update: async ({ resource, ...params }) => {
+	return simpleRestProvider.update({ 
+	  resource: resource === "jobs" ? "blog_posts" : resource,
+	  ...params 
+	});
+  },
+  deleteOne: async ({ resource, ...params }) => {
+	return simpleRestProvider.deleteOne({ 
+	  resource: resource === "jobs" ? "blog_posts" : resource,
+	  ...params 
+	});
+  },
+  getMany: async ({ resource, ...params }) => {
+	return simpleRestProvider.getMany({ 
+	  resource: resource === "jobs" ? "blog_posts" : resource,
+	  ...params 
+	});
   }
-});
+};
