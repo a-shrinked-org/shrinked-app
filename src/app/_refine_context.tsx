635a1a733cb4b4f4f28e0cf04bf3d096ac9204df
damnventures <hi@damn.vc>
2025-02-18 13:15:57 -0500

    ok ship this

--- src/app/_refine_context.tsx
+++ src/app/_refine_context.tsx
@@ -15,6 +15,19 @@ import "@styles/global.css";
 declare module "next-auth" {
   interface Session {
     accessToken?: string;
+    user?: {
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    }
+  }
+
+  interface User {
+    id?: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
   }
 }
 
