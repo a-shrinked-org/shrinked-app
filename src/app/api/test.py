a709ce9b8e0bfbcf398dc5c621c385f3dcfebb21
damnventures <hi@damn.vc>
2025-07-07 01:27:38 -0400

    run

--- src/app/api/test.py
+++ src/app/api/test.py
@@ -1,12 +1,12 @@
 from http.server import BaseHTTPRequestHandler
 import json
 
-class Handler(BaseHTTPRequestHandler):
+class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        response = {"message": "Test successful"}
+        response = {"message": "Test successful", "path": self.path}
         self.wfile.write(json.dumps(response).encode())
-
-handler = Handler
+        return
